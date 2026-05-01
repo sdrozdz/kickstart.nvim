223a6f9644561b26dbc5287c7d172ea94b919a67
Sebastian Drożdż <seba987xdc@gmail.com>
2026-05-01 22:29:37 +0200

    Add nvim-tree plugin

--- init.lua
+++ init.lua
@@ -87,8 +87,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
@@ -921,6 +921,38 @@ require('lazy').setup({
           end
         end,
       })
+    end,
+  },
+
+  { -- Filetree
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup {
+        -- This ensures the tree looks good and has helpful features
+        view = {
+          width = 35,
+          relativenumber = true,
+        },
+        -- Customizations for file management behavior
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+        actions = {
+          open_file = {
+            quit_on_open = true,
+          },
+        },
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        },
+      }
+      -- Optional: Bind a key to toggle the tree
+      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle [E]xplorer' })
     end,
   },
 
